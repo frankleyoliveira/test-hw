bd602f274ea5eb89614e004664fd72a497897722
Frankley Oliveira <frankleyj.o@gmail.com>
2025-09-08 10:34:55 -0300

    add pagination icons

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -20,17 +20,49 @@ export default function Pagination({
         disabled={page === 1}
         className="rounded-md bg-gray-200 hover:bg-gray-300 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
       >
-        Previous
+        <svg
+          data-slot="icon"
+          fill="none"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          ></path>
+        </svg>
       </button>
+
       <span>
         Page {page} of {totalPages}
       </span>
+
       <button
         onClick={() => onPageChange(page + 1)}
         disabled={page === totalPages}
         className="rounded-md bg-gray-200 hover:bg-gray-300 px-3 py-1 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
       >
-        Next
+        <svg
+          data-slot="icon"
+          fill="none"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </button>
     </div>
   )
